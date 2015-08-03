4eb81fdbd961fc6d3fb2aa926412c70954361007
Exosgenesis <exosgenesis@gmail.com>
2015-08-03 15:22:14 +0500

    Add user_vote to likes tag context

--- likes/templatetags/likes_inclusion_tags.py
+++ likes/templatetags/likes_inclusion_tags.py
@@ -1,27 +1,31 @@
-from secretballot.models import Vote
-
 from django import template
 
-from likes.utils import can_vote, likes_enabled
+from likes.utils import can_vote, likes_enabled, user_vote
 
 register = template.Library()
 
 
 @register.inclusion_tag('likes/inclusion_tags/likes_extender.html', takes_context=True)
 def likes(context, obj, template=None):
+
     if template is None:
         template = 'likes/inclusion_tags/likes.html'
+
     request = context['request']
     import_js = False
+
     if not hasattr(request, '_django_likes_js_imported'):
         setattr(request, '_django_likes_js_imported', 1)
         import_js = True
+
     context.update({
         'template': template,
         'content_obj': obj,
         'likes_enabled': likes_enabled(obj, request),
         'can_vote': can_vote(obj, request.user, request),
+        'user_vote': user_vote(obj, request),
         'content_type': "-".join((obj._meta.app_label, obj._meta.module_name)),
         'import_js': import_js
     })
+
     return context
